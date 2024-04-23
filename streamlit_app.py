78c889c786461ee0e1f90463ffaf800dcdd44308
Jawad Rashid <jawadrashid2010@gmail.com>
2024-04-23 19:36:40 +0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,25 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+
+# Write directly to the app
+st.title("Zena's Amazing Athleisure Catalog")
+
+# Get the current credentials
+session = get_active_session()
+
+colors_dataframe = session.sql("SELECT COLOR_OR_STYLE from ZENAS_ATHLEISURE_DB.products.catalog_for_website").collect()
+selected_color = st.selectbox("Pick a sweatsuit color or style:", colors_dataframe)
+
+catalog_dataframe = session.sql("SELECT * from ZENAS_ATHLEISURE_DB.products.catalog_for_website where COLOR_OR_STYLE='" + selected_color + "'").collect()
+st.dataframe(catalog_dataframe)
+
+selected_item = catalog_dataframe[0]
+
+st.image(selected_item["DIRECT_URL"], use_column_width  = "auto", caption="Our warm, comfortable, " + selected_color + " sweatsuit!")
+
+st.write("**Price:** " + str(selected_item["PRICE"]))
+
+st.write("**Sizes Available:** " + selected_item["SIZE_LIST"])
+
+st.write(selected_item["UPSELL_PRODUCT_DESC"])
